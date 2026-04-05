bcf9e7df3d6485d237e2879f9b20b38a89b109d3
Saud Ahmad <200655179+Bulk-Tools@users.noreply.github.com>
2026-04-06 02:39:46 +0500

    Fix formatting and ensure base URL is correct

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,7 +10,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  // If your GitHub repo URL is https://github.com/username/My-Drive-App
-  // This MUST be '/My-Drive-App/'
-  base: '/Google-Drive-Analyzer-/', 
+  // Ensure this matches your exact GitHub repository name
+  base: '/Google-Drive-Analyzer-/',
 })
